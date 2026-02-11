ecfd5b64e72c3602ed49cce551e3584e3acb438e
Afra Anjum <lilithkard77@gmail.com>
2026-02-11 12:19:44 +0600

    added effects on navlinks

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,54 @@
+"use client"
 import Link from "next/link";
+import DecryptedText from "./DecryptedText";
 
-export default function Navbar(){
-    return (
-        <>
-        <nav className="grid text-center divide-x font-semibold font-mono text-xl  grid-cols-4 uppercase">
-            <Link href={'/'} className="pt-3">Home</Link>
-            <Link href={'/projects'} className="pt-3">Projects</Link>
-            <Link href={'/notes'} className="pt-3">Notes</Link>
-            <Link href={'/contact'} className="pt-3">Contacts</Link>
-        </nav>
-        </>
-    )
+export default function Navbar() {
+  return (
+    <>
+      <nav className="grid text-center divide-x font-semibold text-xl  grid-cols-4 uppercase font-mono">
+        <Link href={"/"} className="pt-3">
+          <DecryptedText
+          text="Home"
+          speed={80}
+          animateOn="view"
+          revealDirection="start"
+          sequential
+          useOriginalCharsOnly={false}
+        />
+        </Link>
+        
+
+        <Link href={"/projects"} className="pt-3">
+          <DecryptedText
+          text="Projects"
+          speed={80}
+          animateOn="view"
+          revealDirection="start"
+          sequential
+          useOriginalCharsOnly={false}
+        />
+        </Link>
+        <Link href={"/notes"} className="pt-3">
+          <DecryptedText
+          text="notes"
+          speed={80}
+          animateOn="view"
+          revealDirection="start"
+          sequential
+          useOriginalCharsOnly={false}
+        />
+        </Link>
+        <Link href={"/contact"} className="pt-3">
+          <DecryptedText
+          text="contacts"
+          speed={80}
+          animateOn="view"
+          revealDirection="start"
+          sequential
+          useOriginalCharsOnly={false}
+        />
+        </Link>
+      </nav>
+    </>
+  );
 }
